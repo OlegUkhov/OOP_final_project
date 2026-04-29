ef45d9f417386dcb2b3d8b03618095930b31a6a8
Autumn <autumn@example.com>
2026-04-30 01:02:40 +0500

    fill the User class

--- src/User.java
+++ src/User.java
@@ -1,25 +1,48 @@
+import java.util.Objects;
+
 public abstract class User {
     protected String id;
     protected String firstName;
     protected String lastName;
     protected String email;
     protected String password;
-//    protected Language language;
-    public User(){
-
+    protected Language language;
+    public User(String id, String firstName, String lastName,
+                String email, String password, Language language){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.language = language;
     }
-//    public boolean login(){};
-    public void logout(){};
+    public boolean login(){
+        return true;
+    };
+    public void logout(){
+
+    };
     @Override
     public String toString(){
-//        return
+        return firstName + " " + lastName + " has id " + id + " email " + email;
     }
     @Override
     public boolean equals(Object o){
-
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
     }
     @Override
     public int hashCode(){
+        return Objects.hash(id);
+    }
+    @Override
+    public void update(ResearchPaper paper){
 
+    }
+    //мунда геттерлер сеттерлер жазу кажет
+    public String getId(){
+        return id;
     }
 }

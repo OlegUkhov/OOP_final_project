d3d1fb07f8d5e88caf134d1d3aeb32cdfd0fc5a4
Autumn <autumn@example.com>
2026-04-29 15:15:33 +0500

    create actor classes

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public abstract class User {
+    protected String id;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String password;
+//    protected Language language;
+    public User(){
+
+    }
+//    public boolean login(){};
+    public void logout(){};
+    @Override
+    public String toString(){
+//        return
+    }
+    @Override
+    public boolean equals(Object o){
+
+    }
+    @Override
+    public int hashCode(){
+
+    }
+}

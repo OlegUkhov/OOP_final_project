848b8039a002dcc9e03792375849d2032e44395c
Autumn <autumn@example.com>
2026-05-01 15:56:06 +0500

    implement journal and lesson classes

--- src/Journal.java
+++ src/Journal.java
@@ -1,2 +1,51 @@
-public class Journal {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Journal implements Observable {
+    private String name;
+    private List<ResearchPaper> papers;
+    private List<Observer> subscribers;
+    
+    public Journal(String name) {
+        this.name = name;
+        this.papers = new ArrayList<>();
+        this.subscribers = new ArrayList<>();
+    }
+    
+    public void publishPaper(ResearchPaper p) {
+        papers.add(p);
+        notifyObservers(p);
+    }
+    
+    @Override
+    public void subscribe(Observer o) {
+        if (!subscribers.contains(o)) {
+            subscribers.add(o);
+        }
+    }
+    
+    @Override
+    public void unsubscribe(Observer o) {
+        subscribers.remove(o);
+    }
+    
+    @Override
+    public void notifyObservers(ResearchPaper paper) {
+        for (Observer observer : subscribers) {
+            observer.update(paper);
+        }
+    }
+    
+    public List<ResearchPaper> getPapers() {
+        return new ArrayList<>(papers);
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public String toString() {
+        return "Journal{" + "name='" + name + '\'' + ", papers=" + papers.size() + '}';
+    }
 }
--- src/Lesson.java
+++ src/Lesson.java
@@ -1,2 +1,25 @@
 public class Lesson {
+    private String lessonId;
+    private String topic;
+    private LessonType lessonType;
+
+    public Lesson(String lessonId, String topic, LessonType lessonType) {
+        this.lessonId = lessonId;
+        this.topic = topic;
+        this.lessonType = lessonType;
+    }
+
+    public LessonType getLessonType() {
+        return lessonType;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson{" +
+                "lessonId='" + lessonId + '\'' +
+                ", topic='" + topic + '\'' +
+                ", lessonType=" + lessonType +
+                '}';
+    }
 }
+

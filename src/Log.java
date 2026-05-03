3bccec53208feb764415e7533e550c743eebb88c
Autumn <autumn@example.com>
2026-05-03 11:50:35 +0500

    implement Log

--- src/Log.java
+++ src/Log.java
@@ -1,2 +1,55 @@
+import java.util.Date;
+import java.util.Objects;
+
 public class Log {
+    private String logId;
+    private String userId;
+    private String action;
+    private Date timestamp;
+
+    public Log(String logId, String userId, String action, Date timestamp) {
+        this.logId = logId;
+        this.userId = userId;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
+    public String getLogId() {
+        return logId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "logId='" + logId + '\'' +
+                ", userId='" + userId + '\'' +
+                ", action='" + action + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Log)) return false;
+        Log log = (Log) o;
+        return Objects.equals(logId, log.logId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(logId);
+    }
 }
